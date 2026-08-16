bc0335fe59d06ac0478ec3cb3062acf7256abbbc
tedkid1 <6hr4y29hbd@privaterelay.appleid.com>
2026-08-16 12:03:18 -0400

    Add: python-variable-types - submission-1

---
+++ Python For Beginners/python-variable-types/submission-1.py
@@ -0,0 +1,13 @@
+integer_type = 25
+float_type = 98.5
+bool_type = True
+string_type = 'Alice'
+list_type = [1, 2, 3]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
