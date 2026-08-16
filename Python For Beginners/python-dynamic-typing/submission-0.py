fbb899e675ad3d12c00c0af6d3c374c765af2ee4
tedkid1 <6hr4y29hbd@privaterelay.appleid.com>
2026-08-16 12:30:35 -0400

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+integer_type = 1
+print(type(integer_type))
+float_type = 5.3
+print(type(float_type))
+bool_type = True
+print(type(bool_type))
+str_type = "Alice"
+print(type(str_type))
+list_type = [1, 2, 3]
+print(type(list_type))
